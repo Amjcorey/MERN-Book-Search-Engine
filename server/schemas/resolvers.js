5bbb62686e9c42b655468f89313719ba15113bf3
Amanda Corey <76081915+Amjcorey@users.noreply.github.com>
2024-05-06 13:04:56 -0700

    troubleshoot deployment, one solution was try git rm package-lock.json; format folders and clean up code in server

--- server/schemas/resolvers.js
+++ server/schemas/resolvers.js
@@ -4,79 +4,75 @@ const { signToken, AuthenticationError } = require("../utils/auth");
 const resolvers = {
   Query: {
     me: async (parent, args, context) => {
+      console.log(context.user);
       if (context.user) {
-        return await User.findOne({ _id: context.user._id }).select(
-          "-__v -password"
-        );
+        return User.findOne({ _id: context.user._id });
       }
-      throw AuthenticationError;
+      throw new Error("user not found");
     },
   },
-// #26 and 28 activities reference
-Mutation: {
-  // add user to system
-  addUser: async (parent, { username, email, password }) => {
-    const user = await User.create({ username, email, password });
-    const token = signToken(user);
-    return { token, user };
-  },
-  // login credential validation
-  login: async (parent, { email, password }) => {
-    const user = await User.findOne({ email });
+  // #26 and 28 activities reference
+  Mutation: {
+    // login credential validation
+    login: async (parent, args) => {
+      const user = await User.findOne({ email: args.email });
+      if (!user) {
+        throw AuthenticationError;
+      }
+      const isCorrectPassword = await user.isCorrectPassword(args.password);
+      console.log(!isCorrectPassword);
+      if (!isCorrectPassword) {
+        throw AuthenticationError;
+      }
+      const token = signToken(user);
+      return { token, user };
+    },
+    // add user to system
+    addUser: async (parent, args) => {
+      const user = await User.create(args);
+      const token = signToken(user);
+      return { token, user };
+    },
 
-    if (!user) {
-      throw AuthenticationError;
-    }
-
-    const correctPw = await user.isCorrectPassword(password);
-
-    if (!correctPw) {
-      throw AuthenticationError;
-    }
-
-    const token = signToken(user);
-
-    return { token, user };
-  },
-  //add book to user profile
-  addBook: async (parent, args, context) => {
-    if (context.user) {
-      const newBook = await User.findOneAndUpdate(
-        { _id: context.user._id },
-        {
-          $addToSet: {
-            savedBooks: {
-              description: args.description,
-              author: args.author,
-              title: args.title,
-              image: args.image,
-              bookId: args.bookId,
+    //add book to user profile
+    addBook: async (parent, args, context) => {
+      if (context.user) {
+        const newBook = await User.findOneAndUpdate(
+          { _id: context.user._id },
+          {
+            $addToSet: {
+              savedBooks: {
+                description: args.description,
+                author: args.author,
+                title: args.title,
+                image: args.image,
+                bookId: args.bookId,
+              },
             },
           },
-        },
-        {
-            new: true
-        }
-      );
-      return newBook;
-    }
-    throw new AuthenticationError("User not found");
-  },
+          {
+            new: true,
+          }
+        );
+        return newBook;
+      }
+      throw new AuthenticationError("User not found");
+    },
 
-  // function to delete book from user profile
-  removeBook: async (parent, { bookId }, context) => {
-    console.log("***in removeBook", bookId);
-    if (context.user) {
-      const deleteBook = await User.findOneAndUpdate(
-        { _id: context.user._id },
-        { $pull: { savedBooks: { bookId } } },
-        { new: true }
-      );
-      return deleteBook;
-    }
-    throw new AuthenticationError("User not found");
-  }
-}
+    // function to delete book from user profile
+    removeBook: async (parent, { bookId }, context) => {
+      console.log("***in removeBook", bookId);
+      if (context.user) {
+        const deleteBook = await User.findOneAndUpdate(
+          { _id: context.user._id },
+          { $pull: { savedBooks: { bookId } } },
+          { new: true }
+        );
+        return deleteBook;
+      }
+      throw new AuthenticationError("User not found");
+    },
+  },
 };
 
 module.exports = resolvers;
--- server/server.js
+++ server/server.js
@@ -1,37 +1,51 @@
-const { ApolloServer } = require("@apollo/server");
-const { expressMiddleware } = require("@apollo/server/express4");
-const { typeDefs, resolvers } = require("./schemas");
 const express = require("express");
 const path = require("path");
 const db = require("./config/connection");
-const routes = require("./routes");
+// const routes = require("./routes");
+// const { expressMiddleware } = require("@apollo/server/express4");
+const { ApolloServer } = require("apollo-server-express");
+const { typeDefs, resolvers } = require("./schemas");
 const { authMiddleware } = require("./utils/auth");
 
 const app = express();
-const PORT = process.env.PORT || 3001;
 const server = new ApolloServer({
   typeDefs,
   resolvers,
+  context: authMiddleware,
 });
 
+const PORT = process.env.PORT || 3001;
+
+await server.start(); //start server
+
+app.use(express.urlencoded({ extended: true }));
+app.use(express.json());
+app.use("/graphql", expressMiddleware(server, { context: authMiddleware }));
+
+// if in production, serve client/build as static assets
+if (process.env.NODE_ENV === "production") {
+  app.use(express.static(path.join(__dirname, "../client/build")));
+}
+
+//Serve build/index.html when application is accessed at root
+app.get("/", (req, res) => {
+  res.sendFile(path.join(__dirname, "../client"));
+});
+
+// Create a new instance of an Apollo server with the GraphQL schema
 const startApolloServer = async () => {
-  await server.start(); //start server
-
-  app.use(express.urlencoded({ extended: true }));
-  app.use(express.json());
-  app.use('/graphql', expressMiddleware(server, {context: authMiddleware}));
-
-  // if we're in production, serve client/build as static assets
-  if (process.env.NODE_ENV === "production") {
-    app.use(express.static(path.join(__dirname, "../client/dist")));
-    app.get('*', (req, res) => {
-      res.sendFile(path.join(__dirname, '../client/dist/index.html'));
-    });
-  }
+  await server.start();
+  server.applyMiddleware({ app });
 
   db.once("open", () => {
-    app.listen(PORT, () =>
-      console.log(`🌍 Now listening on localhost:${PORT}`)
-    );
+    app.listen(PORT, () => {
+      console.log(`API server running on port ${PORT}!`);
+      console.log(
+        `Use GraphQL at http://localhost:${PORT}${server.graphqlPath}`
+      );
+    });
   });
 };
+
+// Async function call to start server
+startApolloServer();
